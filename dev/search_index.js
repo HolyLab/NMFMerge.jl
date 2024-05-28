87e3a95d9240632229f886e520993b53f0998f93
Documenter.jl <documenter@juliadocs.github.io>
2024-05-28 21:26:52 +0000

    build based on ca636b5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NMFMerge","category":"page"},{"location":"#NMFMerge","page":"Home","title":"NMFMerge","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NMFMerge.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NMFMerge]","category":"page"}]
+}
