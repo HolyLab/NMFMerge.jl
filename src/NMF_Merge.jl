7eea0a03c6094dee801d43221ca718632542ecb9
youdongguo <1010705897@qq.com>
2024-05-28 13:55:23 -0500

    creat new merge pkg for publication

--- src/NMF_Merge.jl
+++ src/NMF_Merge.jl
@@ -1,5 +1,165 @@
 module NMF_Merge
 
-# Write your package code here.
+using LinearAlgebra, DataStructures
+
+export colnormalize,
+       colmerge2to1pq,
+       mergecol2to1!,
+       mergecolumns
+
+function colnormalize(W, H, p::Int=2)
+    W_vec = [W[:,j] for j in axes(W, 2)]
+    H_vec = [H[i, :] for i in axes(H, 1)]
+    nonzeroindex = findall(x -> x != 0, norm.(W_vec, 2))
+    W_vec_nonzero, H_vec_nonzero= W_vec[nonzeroindex], H_vec[nonzeroindex]
+    W_vec2 = normalize.(W_vec_nonzero, p)
+    H_vec2 = H_vec_nonzero.*norm.(W_vec_nonzero, p)
+    W_in, H_in = hcat(W_vec2...), hcat(H_vec2...)'
+    @assert abs(norm(W_in[:,1], p)-1) <= 1e-12
+    return W_in, H_in
+end
+
+function colmerge2to1pq(S::AbstractArray, T::AbstractArray, n::Integer)
+    mrgseq = Tuple{Integer, Integer}[]
+    S = [S[:, j] for j in axes(S, 2)];
+    T = [T[i, :] for i in axes(T, 1)];
+    for s in S
+        abs(norm(s)-1)<1e-12 || throw(ArgumentError("W columns must be normalized"))
+    end
+    Nt = length(S)
+    Nt >= 2 || throw(ArgumentError("Cannot do 2 to 1 merge: Matrix size smaller than 2"))
+    Nt >= n || throw(ArgumentError("Final solution more than original size"))
+    pq = initialize_pq_2to1(S, T)
+    m = Nt
+    while m > n
+        id0, id1 = dequeue!(pq)
+        while isempty(S[id0])||isempty(S[id1])
+            id0, id1 = dequeue!(pq)
+        end
+        push!(mrgseq, (id0, id1))
+        S, T, id01, _ = mergecol2to1!(S, T, id0, id1);
+        pqupdate2to1!(pq, S, T, id01, 1:id01-1);
+        m -= 1
+    end
+    Smtx, Tmtx = reduce(hcat, filter(!isempty, S)), reduce(hcat, filter(!isempty, T))'
+    return Smtx, Matrix(Tmtx), S, T, mrgseq
+end
+
+function initialize_pq_2to1(S::AbstractVector, T::AbstractVector)
+    err_pq = PriorityQueue{Tuple{Integer, Integer},Float64}()
+    for id0 in length(S):-1:2
+        err_pq = pqupdate2to1!(err_pq, S, T, id0, 1:id0-1)
+    end
+    return err_pq
+end
+
+function pqupdate2to1!(pq, S::AbstractVector, T::AbstractVector, id01::Integer, overlapids::AbstractRange{To}) where To
+    for id in overlapids
+        if !isempty(S[id]) && !isempty(S[id01])
+            loss = solve_remix(S, T, id, id01)[2]
+            enqueue!(pq, (id, id01), loss)    
+        end
+    end
+    return pq
+end
+
+function solve_remix(S, T, id1, id2)
+    τ, δ, c, h1h1, h1h2, h2h2 = build_tr_det(S, T, id1, id2)
+    if h1h1 == 0
+        return c, zero(c), (zero(c),one(c))
+    end
+    if h2h2 == 0
+        return c, zero(c), (one(c),zero(c))
+    end
+    λ_max = τ/2+sqrt(τ^2/4-δ)
+    λ_min = τ/2-sqrt(τ^2/4-δ)
+    ξ = (λ_max-h2h2-c*h1h2)/(h1h2+c*h2h2)
+    u = (ξ, 1)./sqrt(1+ξ^2)
+    p = λ_min
+    return c, p, u
+end
+
+function build_tr_det(W::AbstractVector, H::AbstractVector, id1::Integer, id2::Integer)
+    mW, mH = sdot(W, id1, id2), tdot(H, id1, id2)
+    c = mW[1,2]
+    h1h1 = mH[1,1]
+    h1h2 = mH[1,2]
+    h2h2 = mH[2,2]
+    τ = h1h1+2c*h1h2+h2h2
+    δ = (1-c^2)*(h1h1*h2h2-h1h2^2)
+    return τ, δ, c, h1h1, h1h2, h2h2
+end
+
+function mergecol2to1!(S::AbstractVector, T::AbstractVector, id0::Integer, id1::Integer)
+    S01, T01, loss = mergepair(S, T, id0, id1)
+    S[id0] = S[id1] = T[id0] = T[id1] = eltype(S[1])[]
+    id01 = length(S)+1
+    push!(S, S01)
+    push!(T, T01)
+    return S, T, id01, loss
+end
+
+function mergepair(S::AbstractVector, T::AbstractVector, id1::Integer, id2::Integer)
+    c, loss, u, = solve_remix(S, T, id1, id2)
+    S12, T12 = remix_enact(S, T, id1, id2, c, u)
+    return S12, T12, loss
+end
+
+function remix_enact(S::AbstractVector{TS}, T::AbstractVector, id1::Integer, id2::Integer, c::AbstractFloat, w::Tuple{Tw, Tw}; catT::Bool=false) where {Tw, TS}
+    # Note that catT is the analog of drop_crossterm: both are used
+    # when the time-vectors have no overlap
+    S12 = zeros(eltype(TS), length(S[id1]))
+    S12 += w[1]*S[id1]
+    S12 += w[2]*S[id2]
+    T1, T2 = (w[1]+w[2]*c)*T[id1], (w[1]*c+w[2])*T[id2]
+    T12 = catT ? vcat(T1, T2) : T1+T2
+    return S12, T12
+end
+
+function mergecolumns(W::AbstractArray, H::AbstractArray, mergeseq::AbstractArray; tracemerge::Bool = false)
+    Err = Float64[]
+    S = [W[:, j] for j in axes(W, 2)]
+    T = [H[i, :] for i in axes(H, 1)]
+    STstage = []
+    for mergeids in mergeseq
+        id0, id1 = mergeids
+        if tracemerge
+            push!(STstage, (copy(S), copy(T)))
+        end
+        S, T, _, loss = mergecol2to1!(S, T, id0, id1)
+        err = loss
+        push!(Err, err)
+    end
+    Smtx, Tmtx = hcat(filter(x -> x != [], S)...), hcat(filter(x -> x != [], T)...)'
+    return Smtx, Matrix(Tmtx), STstage, Err
+end
+
+function sdot(S::AbstractVector, id::Vararg{Int, N}) where N
+    mS = zeros(length(id), length(id))
+    for (j, id_j) in enumerate(id)
+        for (i, id_i) in enumerate(id)
+            if i >= j
+                mS[i, j] = min(1.0, S[id_i]'*S[id_j])
+            else
+                mS[i, j] = mS[j, i]
+            end
+        end
+    end
+    return mS
+end
+
+function tdot(T::AbstractVector, id::Vararg{Int, N}) where N
+    mT = zeros(length(id), length(id))
+    for (j, id_j) in enumerate(id)
+        for (i, id_i) in enumerate(id)
+            if i >= j
+                mT[i, j] = T[id_i]'*T[id_j]
+            else
+                mT[i, j] = mT[j, i]
+            end
+        end
+    end
+    return mT
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,96 @@
-using NMF_Merge
+using NMF_Merge, NMF, LinearAlgebra, DataStructures, ForwardDiff
 using Test
 
-@testset "NMF_Merge.jl" begin
-    # Write your tests here.
+function build_Qs(S::AbstractVector, T::AbstractVector, id1::Integer, id2::Integer; drop_crossterm::Bool=false)
+    mS, mT = NMF_Merge.sdot(S, id1, id2), NMF_Merge.tdot(T, id1, id2)
+    c = mS[1,2]
+    τ1τ1 = mT[1,1]
+    τ1τ2 = drop_crossterm ? zero(τ1τ1) : mT[1,2]
+    τ2τ2 = mT[2,2]
+    q1 = τ1τ1 + 2*c*τ1τ2 + c^2*τ2τ2
+    q12 = c*τ1τ1 + (1+c^2)*τ1τ2 + c*τ2τ2
+    q2 = c^2*τ1τ1 + 2*c*τ1τ2 + τ2τ2
+    Q1 = [q1 q12; q12 q2]
+    s1s1 = mS[1,1]
+    s2s2 = mS[2,2]
+    Q2 = [s1s1 c; c s2s2]
+    return Q1, Q2, c, τ1τ1, τ1τ2, τ2τ2
 end
+
+@testset "Single-component image" begin
+    ns = 31
+    nt = 100
+    nthalf = nt>>1
+    w = 7
+    W = exp.(-((1:ns) .- (ns+1)>>1).^2/(2*w^2))
+    H = rand(Float64, 1, nt)
+    img = W*H+eps()*randn(ns, nt)
+
+    W0, H0 = NMF.nndsvd(img, 2)
+    imgnf = NMF.solve!(NMF.CoordinateDescent{Float64}(), img, W0, H0)
+    W1, H1 = imgnf.W, imgnf.H
+    W1n, H1n = colnormalize(W1, H1)
+    [@test abs(norm(W1n[:,j], 2)-1) <= 1e-12 for j in axes(W1n, 2)] 
+
+    W2 = [W1n[:, j] for j in axes(W1n, 2)];
+    H2 = [H1n[i, :] for i in axes(H1n, 1)];
+    Q1, Q2, _, _, _, _ =build_Qs(W2, H2, 1, 2)
+    @test Q1 == Q1'
+    @test Q2 == Q2
+    F = eigen(Q1, Q2)
+    Fvals, Fvecs = F.values::Vector{eltype(Q2)}, F.vectors::Matrix{eltype(Q2)}
+    idx = argmax(Fvals)
+    w = Fvecs[:,idx]
+
+    τ, δ, c, h1h1, h1h2, h2h2 = NMF_Merge.build_tr_det(W2, H2, 1, 2)
+    c, p, u = NMF_Merge.solve_remix(W2, H2, 1, 2)
+    λ_max = τ/2+sqrt(τ^2/4-δ)
+    λ_min = τ/2-sqrt(τ^2/4-δ)
+    
+    @test abs(λ_max - maximum(F.values))<=1e-12
+    @test abs(λ_min - minimum(F.values))<=1e-12
+
+    @test abs(u[1]/u[2] - w[1]/w[2])<1e-3*(u[1]/u[2] + w[1]/w[2])/2
+    
+    w = [u[1], u[2]]
+    @test norm(Q1*w - maximum(F.values)*Q2*w) < 1e-12
+
+    W12, H12, loss = NMF_Merge.mergepair(W2, H2, 1, 2)
+    Err(Hm) = sum(abs2, W12*Hm'-W1*H1)
+    @test norm(ForwardDiff.gradient(Err, H12)) < 1e-12
+
+end
+
+@testset "Merge by min err" begin
+    # Two cells, one is bright and the other dim. The bright cell is split into two tiles that alternate time points
+    S1 = [0.1, 0.5, 0.4, 0.0, 0.0, 0.0]; S1 = S1 / norm(S1);
+    S2 = [0.0, 0.0, 0.1, 0.9, 0.0, 0.0]; S2 = S2 / norm(S2);
+    T1 = rand(20)
+    T1a, T1b = copy(T1), copy(T1)
+    T1a[1:2:end] .= 0
+    T1b[2:2:end] .= 0
+    coef = 0.1
+    N1a = randn(length(S1)); N1a = N1a / norm(N1a) * coef
+    N1b = randn(length(S1)); N1b = N1b / norm(N1b) * coef
+    T2 = zero(T1)
+    T2[15] = 0.25 * sqrt(min(sum(abs2, N1a) * sum(abs2, T1a), sum(abs2, N1b) * sum(abs2, T1b)))
+    
+    W, H = [S1 S1 S2], [T1a'; T1b'; T2']
+    W0, H0 = [S1 S2], [T1'; T2']
+    Wn, Hn = colnormalize(W, H)
+    @test sum(abs2, W*H - Wn*Hn) < 1e-16
+
+    Wm, Hm, _, _, mergids = colmerge2to1pq(copy(Wn), copy(Hn), 2)
+    Wn1 = [Wn[:, j] for j in axes(Wn, 2)];
+    Hn1 = [Hn[i, :] for i in axes(Hn, 1)];
+    Ids = [(1,2), (1,3), (2,3)]
+    loss1 = NMF_Merge.mergepair(Wn1, Hn1, Ids[1][1], Ids[1][2])[end]
+    loss2 = NMF_Merge.mergepair(Wn1, Hn1, Ids[2][1], Ids[2][2])[end]
+    loss3 = NMF_Merge.mergepair(Wn1, Hn1, Ids[3][1], Ids[3][2])[end]
+    i = findmin([loss1, loss2, loss3])[2]
+
+    @test pop!(copy(mergids)) == Ids[i]
+    @test pop!(mergids) == (1,2)
+
+end
+
