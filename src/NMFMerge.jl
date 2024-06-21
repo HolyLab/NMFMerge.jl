8f962157b538f61997088916a21ff0552ada7f48
youdongguo <42158899+youdongguo@users.noreply.github.com>
2024-06-21 11:25:08 -0500

    add top wrapper not test (#10)
    * add top wrapper not test
    
    * add NMF and GSVD deps
    
    ---------
    
    Co-authored-by: youdongguo <1010705897@qq.com>

--- src/NMFMerge.jl
+++ src/NMFMerge.jl
@@ -1,10 +1,28 @@
 module NMFMerge
 
-using LinearAlgebra, DataStructures
+using LinearAlgebra, DataStructures, NMF, GsvdInitialization
 
-export colnormalize,
+export nmfmerge,
+       colnormalize,
        colmerge2to1pq,
        mergecolumns
+
+function nmfmerge(X, ncomponents::Pair{Int,Int}; tol_final=1e-4, tol_intermediate=sqrt(tol_final), W0=nothing, H0=nothing, kwargs...)
+    f = svd(X)
+    if W0 === nothing && H0 === nothing
+        W0, H0 = NMF.nndsvd(X, ncomponents[2], initdata=f)
+    end
+    result_initial = nnmf(X, ncomponents[2]; kwargs..., tol=tol_intermediate, W0=copy(W0), H0=copy(H0))
+    W_initial, H_initial = result_initial.W, result_initial.H
+    kadd = ncomponents[1] - ncomponents[2]
+    W_over_init, H_over_init = overnmfinit(X, copy(W_initial), copy(H_initial), kadd, initdata=f)
+    result_over = nnmf(X, ncomponents[1]; kwargs..., tol=tol_intermediate, W0=copy(W_over_init), H0=copy(H_over_init))
+    W_over, H_over = result_over.W, result_over.H
+    W_over_normed, H_over_normed = colnormalize(W_over, H_over)
+    Wmerge, Hmerge, _ = colmerge2to1pq(W_over_normed, H_over_normed, ncomponents[2])
+    result_renmf = nnmf(X, ncomponents[2]; kwargs..., init=:custom, tol=tol_final, W0=copy(Wmerge), H0=copy(Hmerge))
+    return result_renmf
+end
 
 function colnormalize!(W, H, p::Integer=2)
     for (j, w) in pairs(eachcol(W))
