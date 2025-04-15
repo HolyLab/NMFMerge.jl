fcab694ade432b231b68e5cdfbe954075076f911
youdongguo <1010705897@qq.com>
2025-04-15 14:47:36 -0500

    fix c=0, g=0

--- src/NMFMerge.jl
+++ src/NMFMerge.jl
@@ -148,8 +148,12 @@ function solve_remix(S, T, id1, id2)
     b = sqrt(τ^2/4-δ)
     λ_max = τ/2+b
     λ_min = δ/λ_max
-    ξ = (h1h1-h2h2+2b)/((h1h2+c*h2h2)*2)
-    u = (ξ, 1)./sqrt(1+2ξ*c+ξ^2)
+    if c == 0 && h1h2 == 0
+        u = h1h1 >= h2h2 ? (one(c), zero(c)) : (zero(c), one(c))
+    else
+        ξ = (h1h1-h2h2+2b)/((h1h2+c*h2h2)*2)
+        u = (ξ, 1)./sqrt(1+2ξ*c+ξ^2)
+    end
     return c, λ_min, u
 end
 
