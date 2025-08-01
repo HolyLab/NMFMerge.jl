564b1d58d6391256687dfaa6f07c61b19cae1137
Tim Holy <tim.holy@gmail.com>
2025-08-01 04:16:24 -0500

    Get Makefile working
    This leverages julia#59108 to generate a log file with the information
    needed to create a language binding. As that PR is not yet merged, this
    may be subject to change.
    
    This appears to get things working up through trimming, at which point
    there are many verifier errors that need to be resolved.

--- language_wrappers/lib.jl
+++ language_wrappers/lib.jl
@@ -8,10 +8,21 @@ struct ReturnValue
     objvalue::Float64
 end
 
-Base.@ccallable function nmfmerge_inplace(Wout::Matrix{Float64}, Hout::Matrix{Float64}, X::Matrix{Float64}, ncomponents::Int32, tol::Float64, maxiter::Int32)::ReturnValue
+struct CMatrix{T}
+    data::Ptr{T}
+    rows::Int32
+    cols::Int32
+end
+
+Base.@ccallable function nmfmerge_inplace(Wout::CMatrix{Float64}, Hout::CMatrix{Float64}, X::CMatrix{Float64}, ncomponents::Int32, tol::Float64, maxiter::Int32)::ReturnValue
+    Wout.rows == X.rows || throw(ArgumentError("Wout and X must have the same number of rows"))
+    Hout.cols == X.cols || throw(ArgumentError("Hout and X must have the same number of columns"))
+    W, H, X = unsafe_wrap(Array, Wout.data, (Wout.rows, Wout.cols)),
+              unsafe_wrap(Array, Hout.data, (Hout.rows, Hout.cols)),
+              unsafe_wrap(Array, X.data, (X.rows, X.cols))
     result = nmfmerge(X, ncomponents; tol_final=tol, maxiter)
-    Wout .= result.W
-    Hout .= result.H
+    W .= result.W
+    H .= result.H
     return ReturnValue(result.niters, result.converged, result.objvalue)
 end
 
